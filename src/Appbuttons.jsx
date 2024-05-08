240811c3e234d105f9d80506540395cdbc7ad65d
Daniel Kilders <danielkildersdiaz@gmail.com>
2024-05-08 19:45:27 +0200

    updated text

--- src/Appbuttons.jsx
+++ src/Appbuttons.jsx
@@ -6,10 +6,10 @@ import {
   import { motion } from "framer-motion";
   
   export default function Buttons () {
-    const IOSUrl = "https://apps.apple.com/us/app/expo-go/id982107779";
+    const IOSUrl = "https://apps.apple.com/us/app/id6502048405";
   
     return (
-      <motion.div className="flex place-content-center py-20"
+      <motion.div className="flex place-content-center py-10"
         whileHover={{ scale: 1.2 }}
         whileTap={{
         scale: 0.8,
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,17 +3,12 @@ import { motion } from "framer-motion";
 import Buttons from "./Appbuttons";
 
 const features = [
-  {
-    name: 'Push to deploy. ',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates. ',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
-  },
+  // {
+  //   name: 'Push to deploy. ',
+  //   description:
+  //     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+  //   icon: CloudArrowUpIcon,
+  // },
 ]
 
 
@@ -25,13 +20,12 @@ export default function Hero() {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base text-center lg:text-left font-semibold leading-7 text-sky-600">Estudia faster</h2>
+              <h2 className="text-base text-center lg:text-left font-semibold leading-7 text-sky-600">Flashcard magic ✨</h2>
               <p className="mt-2 text-3xl font-bold text-center lg:text-left tracking-tight text-gray-900 sm:text-4xl">SwiftStudy</p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
+              Elevate your memory game with SwiftStudy, a flashcards app to memorise anything and test your knowledge in a rewarding way.
               </p>
-              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+              <dl className="max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {features.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
                     <dt className="inline font-semibold text-gray-900">
